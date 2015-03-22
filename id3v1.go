a70a80574e2f06eacb81b6ecb0ea59ebf0c3295c
kraii <matthewkerry@gmail.com>
2015-03-22 14:13:58 -0400

    Initial checkin

---
+++ id3v1.go
@@ -0,0 +1,48 @@
+package id3v1
+
+import (
+	"io"
+	"bytes"
+)
+
+type Id3v1Tag struct {
+	title, artist, album, comment, year string
+	trackNumber int
+}
+
+func ReadTag(r io.ReadSeeker) Id3v1Tag {
+	tagBytes := make([] byte, 128)
+	r.Seek(-128, 2)
+	r.Read(tagBytes)
+
+	header := string(tagBytes[:3])
+	if header != "TAG" { 
+		panic("nooO!")
+	} else {
+		return createTag(tagBytes)
+	} 
+}
+
+func createTag(tagBytes []byte) Id3v1Tag {
+	tag := new(Id3v1Tag)
+	tag.title = trimmedString(tagBytes[3:33])
+	tag.artist = trimmedString(tagBytes[33:63])
+	tag.album = trimmedString(tagBytes[63:93])
+	tag.year = trimmedString(tagBytes[93:97])
+	comment, trackNo := readCommentAndOrTrackNo(tagBytes[97:127])
+	tag.comment = comment
+	tag.trackNumber = trackNo
+	return *tag
+}
+
+func trimmedString(b []byte) string {
+	return string(bytes.TrimRight(b, "\x00"))
+}
+
+func readCommentAndOrTrackNo(b []byte) (string, int) {
+	if b[28] == 0 {
+		return trimmedString(b[:29]), int(b[29])
+	} else {
+		return trimmedString(b), -1
+	}
+}
---
+++ id3v1_test.go
@@ -0,0 +1,35 @@
+package id3v1
+
+import (
+	"testing"
+	"os"
+	. "gopkg.in/check.v1"
+)
+
+func TestId3s(t *testing.T) { TestingT(t) }
+
+type Id3v1TagSuite struct{}
+
+var _ = Suite(&Id3v1TagSuite{})
+var file *os.File
+
+func (s *Id3v1TagSuite) SetUpTest(c *C) {
+	var err error
+	file, err = os.Open("_testdata/spice.mp3")
+	c.Assert(err, Equals, nil)
+}
+
+func (s *Id3v1TagSuite) TestReadWellFormedTag(c *C) {
+	tag := ReadTag(file)
+    c.Check(tag.artist, Equals, "Xander")
+    c.Check(tag.title, Equals, "Spice")
+    c.Check(tag.album, Equals, "Things")
+    c.Check(tag.year, Equals, "2015")
+    c.Check(tag.comment, Equals, "say -v Xander")
+    c.Check(tag.trackNumber, Equals, 1)
+
+}
+
+func (s *Id3v1TagSuite) TearDownTest(c *C) {
+	file.Close()
+}
